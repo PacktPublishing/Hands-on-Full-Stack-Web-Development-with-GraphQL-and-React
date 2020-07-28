cd50d80805e4d6cce0a7279f861004dafce2eaa5
Sebastian Grebe <sebigrebe@googlemail.com>
2020-07-28 22:03:20 +0200

    Update webpack.client.config.js

--- Chapter01/webpack.client.config.js
+++ Chapter01/webpack.client.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const outputDirectory = 'dist';
+const buildDirectory = 'dist';
+const outputDirectory = buildDirectory + '/client';
 module.exports = {
     mode: 'development',
     entry: './src/client/index.js',
